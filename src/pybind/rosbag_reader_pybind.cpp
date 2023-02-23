d6bc2a879ff223bf82e0d85294c0098d4d7984f2
Saurabh Gupta <saurabh.gupta1002@gmail.com>
2023-02-23 16:00:52 +0100

    Start migration

---
+++ src/pybind/rosbag_reader_pybind.cpp
@@ -0,0 +1,15 @@
+#include <pybind11/pybind11.h>
+
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
+int add(int i, int j) { return i + j; }
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(_core, m) {
+    m.def("add", &add, R"pbdoc(
+        Add two numbers
+        Some other explanation about the add function.
+    )pbdoc");
+}
